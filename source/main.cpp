c7ddebe936b6b10aa20ae41dedb86bfd0e9e540c
Álex Lopez <33585530+alorodri@users.noreply.github.com>
2021-05-07 21:12:11 +0200

    lesson 02, methods

--- source/main.cpp
+++ source/main.cpp
@@ -5,18 +5,30 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+bool init();
+bool loadMedia();
+void close();
+
+SDL_Window* gWindow = nullptr;
+// surface contained by the window
+SDL_Surface* gScreenSurface = nullptr;
+// image to load
+SDL_Surface* gImage = nullptr;
+
 int main(int argc, char *argv[]) {
+	init();
+	loadMedia();
+	close();
+	return 0;
+}
 
-	SDL_Window* window = nullptr;
-
-	// surface contained by the window
-	SDL_Surface* screenSurface = nullptr;
-
+bool init() {
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
 		std::cout << "SDL video couldn't initialize, SDL Error: " << SDL_GetError() << "\n";
+		return false;
 	}
 	else {
-		window = SDL_CreateWindow(
+		gWindow = SDL_CreateWindow(
 			"SDL Tutorial",
 			SDL_WINDOWPOS_UNDEFINED,
 			SDL_WINDOWPOS_UNDEFINED,
@@ -25,19 +37,23 @@ int main(int argc, char *argv[]) {
 			SDL_WINDOW_SHOWN
 		);
 
-		if (!window) {
+		if (!gWindow) {
 			std::cout << "Window couldn't be created, SDL Error: " << SDL_GetError() << "\n";
+			return false;
 		}
 		else {
-			screenSurface = SDL_GetWindowSurface(window);
-			SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
-			SDL_UpdateWindowSurface(window);
-			SDL_Delay(2000);
-
-			SDL_DestroyWindow(window);
-			SDL_Quit();
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
 		}
 	}
 
-	return 0;
+	return true;
+}
+
+void close() {
+	SDL_DestroyWindow(gWindow);
+	SDL_Quit();
+}
+
+bool loadMedia() {
+	return true;
 }

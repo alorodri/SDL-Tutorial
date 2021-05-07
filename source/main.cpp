fa28b2c3f99ae8ec5219f821896cb1040f08e0c7
Álex Lopez <33585530+alorodri@users.noreply.github.com>
2021-05-07 21:01:40 +0200

    Creating main tutorial window

--- source/main.cpp
+++ source/main.cpp
@@ -2,6 +2,42 @@
 
 #include <iostream>
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 int main(int argc, char *argv[]) {
+
+	SDL_Window* window = nullptr;
+
+	// surface contained by the window
+	SDL_Surface* screenSurface = nullptr;
+
+	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+		std::cout << "SDL video couldn't initialize, SDL Error: " << SDL_GetError() << "\n";
+	}
+	else {
+		window = SDL_CreateWindow(
+			"SDL Tutorial",
+			SDL_WINDOWPOS_UNDEFINED,
+			SDL_WINDOWPOS_UNDEFINED,
+			SCREEN_WIDTH,
+			SCREEN_HEIGHT,
+			SDL_WINDOW_SHOWN
+		);
+
+		if (!window) {
+			std::cout << "Window couldn't be created, SDL Error: " << SDL_GetError() << "\n";
+		}
+		else {
+			screenSurface = SDL_GetWindowSurface(window);
+			SDL_FillRect(screenSurface, nullptr, SDL_MapRGB(screenSurface->format, 0xFF, 0xFF, 0xFF));
+			SDL_UpdateWindowSurface(window);
+			SDL_Delay(2000);
+
+			SDL_DestroyWindow(window);
+			SDL_Quit();
+		}
+	}
+
 	return 0;
 }
